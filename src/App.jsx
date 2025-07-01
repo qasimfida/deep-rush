ded8e2228810ade257a7486c98369ef65ecae7ca
Qasim Fida <qasimfida0054@gmail.com>
2025-07-01 20:43:15 +0500

    deep-rush design

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import rush from './assets/rush.png'
+import bg from "./assets/bg.png"
+import logo from "./assets/logo.png"
+
+
+function App() {
+  return (
+    <>
+
+      <div className={`flex w-screen min-h-screen  font-helvetica-neue bg-black `}
+      >
+        <div className="w-full px-5">
+          <div className="py-6 ">
+            <img src={logo} alt="deepfi" />
+          </div>
+          <div className="">
+            <div className="relative z-10 flex flex-col items-center text-center md:mt-[144px] mt-16 ">
+              <div className="flex items-center overflow-hidden content">
+                <div class="spotlight"></div>
+                <h2 className="font-bold deep-text ">DeepRush</h2>
+              </div>
+
+              <p className="text-white md:text-[22px] text-xl font-normal mb-10 mt-6 ">
+                Race to win Solana</p>
+              <button className="relative z-10 border border-white rounded-full px-6 md:h-[60px] h-10 text-white text-lg font-normal  transition-colors duration-200 hover:text-[#ACAFBB] flex items-center shadow-light">
+                Revving Soon
+              </button>
+            </div>
+            <img
+              src={rush}
+              alt="Games Animation"
+              className="z-0 w-[888px] max-w-full mx-auto relative left-0 right-0 bottom-0 md:-mt-7"
+            />
+            <footer className="fixed md:bottom-12 bottom-6 left-0 right-0 mx-auto z-10 flex flex-wrap items-center justify-center w-full gap-3 py-4 text-sm font-normal tracking-wide text-center text-[#ACAFBB] md:flex-nowrap md:flex">
+              <p>©2025 DeepRush, a DeepFi Project.</p>
+              <p>All Rights Reserved.</p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
